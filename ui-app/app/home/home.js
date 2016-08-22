94bbedafa24adf50ba83e687701f6fe8ad9517a0
Priyabrata Kuanr <priyabrata.kuanr@target.com>
2016-08-22 11:45:55 +0530

    fixed ui checkin prob

---
+++ ui-app/app/home/home.js
@@ -0,0 +1,56 @@
+(function () {
+	'use strict';
+
+	angular.module('searchdemo.home', ['ngRoute', 'ngMaterial'])
+	.config(['$routeProvider', function($routeProvider) {
+	  $routeProvider.when('/home', {
+	    templateUrl: 'home/home.html',
+	    controller: 'HomeCtrl'
+	  });
+	}]).controller('HomeCtrl', HomeCtrl);
+
+	function HomeCtrl ($timeout, $q, $log, $http) { 
+	 	var self = this; 
+	    self.isDisabled    = false; 
+	    self.matches   = fetchMatches2;
+	    self.searchResults   = [];
+	    self.selectedItemChange = selectedItemChange;
+	    self.searchTextChange   = searchTextChange;
+	    self.fetchMatches2   = fetchMatches2;
+	    self.searchHotels = searchHotels;
+	    // ******************************
+	    // Internal methods
+	    // ******************************
+	    function searchTextChange(prefix) {
+	    // 	 self.matches.length = 0;
+	    // 	 var url =  "http://localhost:9090/autocomplete/" + prefix +"?callback=JSON_CALLBACK"
+ 			 // return $http.jsonp(url).success(function(data){
+ 			 // 	return data.matches;
+ 			 // });
+	    }
+
+	    function fetchMatches2(prefix) { 
+	    	 var deferred = $q.defer()
+	    	 var url =  "http://localhost:9090/autocomplete/" + prefix +"?callback=JSON_CALLBACK"
+ 			 $http.jsonp(url).success(function(data){ 
+ 			 	deferred.resolve(data.matches);
+ 			 });
+ 			 return deferred.promise;
+	    }
+
+	    function searchHotels(searchText) {
+	    	 var url =  "http://localhost:9090/search/" +  searchText +"?callback=JSON_CALLBACK"
+	    	 self.searchResults.length = 0;
+ 			 $http.jsonp(url).success(function(data){
+ 			 	angular.forEach(data.hotels, function(value) {
+				  this.push(value);
+				}, self.searchResults ); 
+		      });  
+	    }
+	    function selectedItemChange(item) {
+	      //$log.info('Item changed to ' + JSON.stringify(item));
+	    }
+	     
+	     
+	}
+})();
